f1826fec1b4475a356c40de353f160998b894abf
Mathieu PATUREL <australie.p@gmail.com>
2019-02-17 19:53:42 +1100

    Use semaphore in net.py
    Having a global net.py might not be the best solution. I don't have
    any cleaner idea though... Maybe instantiating a class per stream, which
    would just be a wrapper...

--- net.py
+++ net.py
@@ -8,13 +8,21 @@ log = getLogger()
 
 # TODO: user trio.Semaphore
 
+# idieally, we have more than just a semaphore: it would also need to check
+# that it could be writing on 2 different stream "at the same time" without any
+# problem. But, in this program, there is only one stream, so it's the exact
+# same, with a tiny bit less over head.
+
 class ConnectionClosed(Exception):
     pass
 
 _reading_deque = deque()
 
+read_semaphor = trio.Semaphore(1)
+
 async def read(stream):
     if len(_reading_deque) == 0:
+        await read_semaphor.acquire()
         # load from actual stream
         data = await stream.receive_some(BUFSIZE)
         log.debug(f"Recieve: {data!r}")
@@ -24,6 +32,7 @@ async def read(stream):
 
         for s in str(data, encoding='utf-8').strip().split('\n'):
             _reading_deque.append(json.loads(s))
+        read_semaphor.release()
     else:
         # make sure that this function is a check point, ie. it doesn't hogg
         # the run loop
@@ -31,7 +40,11 @@ async def read(stream):
         await trio.sleep(0)
     return _reading_deque.popleft()
 
+write_semaphore = trio.Semaphore(1)
+
 async def write(stream, object):
+    await write_semaphore.acquire()
     log.debug(f"Sending {object}")
     await stream.send_all(bytes(json.dumps(object) + '\n', encoding='utf-8'))
+    write_semaphore.release()
     
