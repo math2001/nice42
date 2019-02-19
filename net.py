0dfbb30a6514d6f24a7aa0b16d06d09d4d250d9d
Mathieu PATUREL <australie.p@gmail.com>
2019-02-19 17:56:39 +1100

    Improve organization of the server

--- net.py
+++ net.py
@@ -6,6 +6,8 @@ from constants import *
 
 log = getLogger()
 
+# TODO: test that! it should be easy and fun with trio
+
 class ConnectionClosed(Exception):
     pass
 
@@ -13,42 +15,52 @@ class JSONStream:
     """ A wrapper around a trio.Stream """
 
     def __init__(self, stream):
-        self.stream = stream
-        self._reading_deque = deque()
-        self._read_semaphore = trio.Semaphore(1)
+        self._stream = stream
+
         self._write_semaphore = trio.Semaphore(1)
 
+        # blocks reading from stream and _read_buf at the same time
+        self._read_semaphore = trio.Semaphore(1)
+
+        self._read_buf = bytearray()
+
     async def read(self):
-        if len(self._reading_deque) == 0:
-            log.debug("Acquiring writing semaphore")
+        i = self._read_buf.find(b'\n')
+        if i == -1:
+            log.debug("Aquire reading semaphore")
             await self._read_semaphore.acquire()
-            log.debug("Reading...")
 
-            try:
-                data = await self.stream.receive_some(BUFSIZE)
-            except trio.BrokenResourceError:
-                raise ConnectionClosed(f"reading from {self.stream}")
-            log.debug(f"Receive: {data!r}")
-            self._read_semaphore.release()
+            while i == -1:
+                data = await self._stream.receive_some(BUFSIZE)
+                log.debug(f"Adding to buffer {data!r}")
+                if not data:
+                    raise ConnectionClosed("stream closed")
+                self._read_buf += data
+                i = data.find(b'\n')
+            i += len(self._read_buf)
 
-            if not data:
-                raise ConnectionClosed(f"reading from {self.stream}")
+        # we found a line feed!
+        string = str(self._read_buf[:i], encoding='utf-8')
+        log.debug(f"Read line {string!r}")
+        self._read_buf[:i] = []
+        self._read_semaphore.release()
+        log.debug("Release reading semaphore")
+        return json.loads(string)
 
-            for s in str(data, encoding='utf-8').strip().split('\n'):
-                self._reading_deque.append(json.loads(s))
-        else:
-            # make sure that this function is a check point, ie. it doesn't hogg
-            # the run loop
-            log.debug("Reading from the cache")
-            await trio.sleep(0)
-        return self._reading_deque.popleft()
 
     async def write(self, object):
         log.debug("Acquiring writing semaphore")
         await self._write_semaphore.acquire()
         log.debug(f"Sending {object}")
         try:
-            await self.stream.send_all(bytes(json.dumps(object) + '\n', encoding='utf-8'))
+            await self._stream.send_all(bytes(json.dumps(object) + '\n', encoding='utf-8'))
         except trio.BrokenResourceError:
-            raise ConnectionClosed(f"writing on {self.stream}")
+            raise ConnectionClosed(f"writing on {self._stream}")
         self._write_semaphore.release()
+
+    async def aclose(self):
+        await self._write_semaphore.acquire()
+        await self._read_semaphore.acquire()
+        await self._stream.aclose()
+        self._write_semaphore.release()
+        self._read_semaphore.release()
--- server/server.py
+++ server/server.py
@@ -1,97 +1,11 @@
 import trio
-import time
-import random
-import net
-from server.player import Player
 from log import getLogger
-from itertools import count
-from constants import *
-
-# TODO: change this per player
-REFRESH_RATE = .1 # s
-
-players = {}
-lps = None
+from server.game import Game
 
 log = getLogger()
 
-PLAYER_COUNT = count()
-
-async def send_game_state_forever():
-    while True:
-        for player in players.values():
-            await player.stream.write({
-                "type": "update",
-                "players": [p.serializable for p in players.values() if p.is_on_map],
-                "lps": lps
-            })
-        await trio.sleep(REFRESH_RATE)
-
-async def handle_client(player):
-    log.debug("Waiting for name")
-    await player.get_name()
-    player.spawn((
-        random.randint(0, MAP_SIZE[0] - PLAYER_SIZE[0]),
-        random.randint(0, MAP_SIZE[1] - PLAYER_SIZE[1]),
-    ))
-    log.info(f"{player} added to players dict")
-    players[player.id] = player
-
-    async with trio.open_nursery() as n:
-        # if the client leaves, there's 2 different thing that can happen:
-        # 1. I try to read from a dead connection
-        # 2. I try to write to a dead connection
-        # these raise 2 different errors, which means that both of them have
-        # to be handled
-        n.start_soon(player.get_user_input_forever)
-        n.start_soon(send_game_state_forever)
-
-async def server(stream):
-    log.info("New connection")
-
-    player = Player(next(PLAYER_COUNT), net.JSONStream(stream))
-
-    try:
-        await handle_client(player)
-    except net.ConnectionClosed:
-        # we couldn't read or we couldn't write
-        log.info(f"{player} connection closed")
-    except Exception as e:
-        log.exception("Handler crashed")
-    finally:
-        log.info(f"{player} removed")
-        del players[player.id]
-
-async def gameloop(nursery):
-    global lps
-    log.info("Start game loop")
-    # use to calculate the number of loops per second
-    loops = 0
-    last = time.time()
-    while True:
-        loops += 1
-        if time.time() - last >= 1:
-            lps = loops
-            loops = 0
-            last = time.time()
-
-        for player in players.values():
-
-            player.move()
-
-            # if 2 player collide, one of them has to die
-            # keep shit simple for now. Random'll do
-            for target in players.values():
-                if target is player:
-                    continue
-                if player.collides(target):
-                    if random.randint(0, 1) == 0:
-                        nursery.start_soon(player.killed)
-                    else:
-                        nursery.start_soon(target.killed)
-        await trio.sleep(0.01)
-
 async def run():
-    async with trio.open_nursery() as n:
-        n.start_soon(trio.serve_tcp, server, PORT)
-        n.start_soon(gameloop, n)
+    log.info("Star server")
+    async with trio.open_nursery() as nursery:
+        Game(nursery)
+    log.info("Exiting server")
