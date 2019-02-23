f48ec53ae185c8aaf1fe94fa8a2380543c72a3ca
Mathieu PATUREL <australie.p@gmail.com>
2019-02-23 15:23:55 +1100

    Implement animations of change in position
    It ended up working without any prediction, which means my original idea
    of "buffering" the updates (being late of 1 update) works

--- client/player.py
+++ client/player.py
@@ -6,9 +6,15 @@ from client.utils import *
 
 log = logging.getLogger(__name__)
 
-DEBUG_PREDICTED_POSITION = 1 << 0
+DEBUG_SERVER_POSITION = 1 << 0
+DEBUG_NO_PLAYER = 1 << 1
+DEBUG_PREDICTED_POSITION = 1 << 2
 
-DEBUG = DEBUG_PREDICTED_POSITION
+DEBUG = DEBUG_SERVER_POSITION | DEBUG_PREDICTED_POSITION
+DEBUG = 0
+
+# by how much we predict the position and move to it
+PREDICTION = 0
 
 class Player:
 
@@ -22,12 +28,12 @@ class Player:
 
         self.rect = pygame.Rect((0, 0), PLAYER_SIZE)
 
-        # the predicted change in position
-        self.predicted_change = None
+        # the predicted position
+        self.predicted_pos = None
 
         self.last_server_update = time.time()
 
-    def update_state(self, pos):
+    def update_state(self, new_server_pos):
         """ Update state from the server 
 
         Rule #1:
@@ -42,46 +48,60 @@ class Player:
 
         Whatever happens, I have to get to the predicted position  exactly on
         the next server update.
-
         """
-        direction = [
-            arg(pos[0] - self.server_pos[0]),
-            arg(pos[1] - self.server_pos[1])
-        ]
-        self.last_pos = self.pos
-        self.server_pos = pos
-        self.predicted_change = [
-            direction[0] * PLAYER_SPEED * SERVER_REFRESH_RATE,
-            direction[1] * PLAYER_SPEED * SERVER_REFRESH_RATE,
-        ]
 
         self.last_server_update = time.time()
 
+        self.predicted_pos = [
+            new_server_pos[0] + (new_server_pos[0] - self.server_pos[0]) * PREDICTION,
+            new_server_pos[1] + (new_server_pos[1] - self.server_pos[1]) * PREDICTION
+        ]
+
+        self.server_pos = new_server_pos
+
+
+        # direction = [
+        #     arg(new_server_pos[0] - self.server_pos[0]),
+        #     arg(new_server_pos[1] - self.server_pos[1])
+        # ]
+        # self.last_pos = self.pos
+        # self.server_pos = new_server_pos
+
+        # predicted_change = [
+        #     direction[0] * PLAYER_SPEED * SERVER_REFRESH_RATE,
+        #     direction[1] * PLAYER_SPEED * SERVER_REFRESH_RATE,
+        # ]
+
+        # self.prediction = 
+
+
     def update(self):
         """ Update state in between server updates """
-        if self.predicted_change:
+        if self.predicted_pos:
             percentage_done = (time.time() - self.last_server_update) / SERVER_REFRESH_RATE
-            self.pos = [
-                self.last_pos[0] + percentage_done * self.predicted_change[0],
-                self.last_pos[1] + percentage_done * self.predicted_change[1]
-            ]
+            dx = percentage_done * (self.predicted_pos[0] - self.pos[0])
+            dy = percentage_done * (self.predicted_pos[1] - self.pos[1])
+            self.pos = [self.pos[0] + dx, self.pos[1] + dy]
+
         self.rect.left = int(round(self.pos[0]))
         self.rect.top = int(round(self.pos[1]))
-
 
     def render(self):
         if self.pos is None:
             log.warning(f"{self} position is None")
             return
 
-        pygame.draw.rect(Screen.surface, self.color, self.rect)
-        if DEBUG & DEBUG_PREDICTED_POSITION:
-            pygame.draw.rect(
-                Screen.surface,
-                self.color,
-                pygame.Rect(self.server_pos, PLAYER_SIZE),
-                1
-            )
+        if not DEBUG & DEBUG_NO_PLAYER:
+            pygame.draw.rect(Screen.surface, self.color, self.rect)
+
+        if DEBUG & DEBUG_SERVER_POSITION:
+            pygame.draw.rect(Screen.surface, self.color,
+                             pygame.Rect(self.server_pos, PLAYER_SIZE), 1)
+
+        if DEBUG & DEBUG_PREDICTED_POSITION and self.predicted_pos:
+            r = pygame.Rect(self.predicted_pos, PLAYER_SIZE)
+            pygame.draw.rect(Screen.surface, self.color, r, 1)
+            pygame.draw.rect(Screen.surface, self.color, r.inflate(-4, -4), 1)
 
     def __str__(self):
         return f"<c.Player {self.username!r} {self.pos}>"
